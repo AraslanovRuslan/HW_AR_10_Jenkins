4b96f2afa92e1cb6b1ebb6dd367bec8fa2437dd7
r.araslanov <araslanov.08@gmail.com>
2022-12-12 13:42:29 +0500

    5

--- src/test/java/tests/TestBase.java
+++ src/test/java/tests/TestBase.java
@@ -12,6 +12,7 @@ public class TestBase {
     static void setUp(){
         Configuration.baseUrl = "https://demoqa.com";
         Configuration.browserSize = "1920x1080";
+        Configuration.timeout = 10000;
         Configuration.remote = "https://user1:1234@selenoid.autotests.cloud/wd/hub";
 
     }
